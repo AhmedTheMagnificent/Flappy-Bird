80bd4762d7cfe809d7d84372b5891cc859569cd2
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-12-30 22:39:32 +0500

    added loop for no purpose

--- workspace/src/Main.java
+++ workspace/src/Main.java
@@ -1,7 +1,12 @@
-import org.lwjgl.Version;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("LWJGL Version: " + Version.getVersion());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int number = scanner.nextInt();
+        for(int i = 0; i < 1000; i++) {
+            System.out.printf("%d x %d = %d", number, i, number * i);
+        }
     }
 }
